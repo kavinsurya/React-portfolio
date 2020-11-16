59d66b88bb95a72e907c3f61c889ae21e9e7fefc
kavinsurya <kavinsurya002@gmail.com>
2020-11-16 16:20:58 +0530

    changes

--- src/home.jsx
+++ src/home.jsx
@@ -1,10 +1,10 @@
 import './App.css';
-import image from './img.jpg';
+import logo from './img.jpg';
 
 export function Home() {
     return (
         <div className="container">
-            <img className="image" src={image} alt="Background-image" />
+            <img className="image" src={logo} alt="Background" />
             <div className="text-block">
                 <p className="text-blockName" >Hi,I'm kavin</p>
                 <p className="text-blockRole">Full Stack Developer</p>
