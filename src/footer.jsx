bda31f7405c89deaae8b05e80c72a59e9589d673
kavinsurya <kavinsurya002@gmail.com>
2020-11-17 09:43:47 +0530

    footer changes

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,12 +1,22 @@
-import './App.css';
+import "./App.css";
+import { MdCall, MdEmail } from "react-icons/md";
 
 export function Footer() {
-    return (
-        <div className="footer">
-            <p className="footerName">Kavinsurya &copy; 2020</p>
-        </div>
-    )
+  return (
+    <div className="footer">
+      <h3 className="center">Contact</h3>
+      <h3 className="center">
+        <MdCall size={30} /> +91-9790234646
+      </h3>
+      <h3 className="center">
+        <MdEmail size={30} />
+        <a href="mailto:kavinsurya002@gmail.com"> kavinsurya002@gmail.com</a>
+      </h3>
+<hr/>
+      <p className="footerName">Kavinsurya &copy; 2020</p>
+      <hr/>
+    </div>
+  );
 }
 
-
-export default  Footer ;
+export default Footer;
--- src/header.jsx
+++ src/header.jsx
@@ -4,13 +4,6 @@ export function Header() {
     return (
         <div className="header">
             <a href="#default" className="logo">Kavinsurya</a>
-            <div className="header-right">
-                <a href="#home">Home</a>
-                <a href="#about">About</a>
-                <a href="#education">Education</a>
-                <a href="#contact">Contact</a>
-
-            </div>
         </div>
     );
 }
